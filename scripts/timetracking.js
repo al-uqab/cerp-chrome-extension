3a79bba17be605727d8b01a7f182be9a9b3173c3
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-12-31 12:36:51 +0200

    Refactor timetracking

--- scripts/timetracking.js
+++ scripts/timetracking.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+"use strict"
+
+document.addEventListener('DOMContentLoaded', () => {
     const hourDisplay = document.getElementById('hour');
     const minuteDisplay = document.getElementById('minutes');
     const secondDisplay = document.getElementById('seconds');
@@ -9,104 +11,107 @@ document.addEventListener('DOMContentLoaded', function() {
     let timerInterval;
     let isTimerRunning = false;
     let startTime;
-    let pausedTime = 0; // Variable to store time when paused
+    let pausedTime = 0;
 
-    function startTimer(taskTitle) {
-        const initialTime = pausedTime; // Use paused time as the initial time
+    const updateTimerDisplay = () => {
+        const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+        const totalSeconds = pausedTime + elapsedTime;
 
-        timerInterval = setInterval(() => {
-            const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
-            const totalSeconds = initialTime + elapsedTime;
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
 
-            const hours = Math.floor(totalSeconds / 3600);
-            const minutes = Math.floor((totalSeconds % 3600) / 60);
-            const secs = totalSeconds % 60;
+        hourDisplay.innerText = String(hours).padStart(2, '0');
+        minuteDisplay.innerText = String(minutes).padStart(2, '0');
+        secondDisplay.innerText = String(seconds).padStart(2, '0');
+    };
 
-            hourDisplay.innerText = String(hours).padStart(2, '0');
-            minuteDisplay.innerText = String(minutes).padStart(2, '0');
-            secondDisplay.innerText = String(secs).padStart(2, '0');
-        }, 1000);
-    }
+    const startTimer = (taskTitle) => {
+        const initialTime = pausedTime;
+        timerInterval = setInterval(updateTimerDisplay, 1000);
+    };
 
-    function pauseTimer() {
+    const pauseTimer = () => {
         clearInterval(timerInterval);
         pausedTime += Math.floor((Date.now() - startTime) / 1000);
         isTimerRunning = false;
-        chrome.storage.local.set({ 'pausedTime': pausedTime }); // Save paused time to storage
-    }
+        chrome.storage.local.set({'pausedTime': pausedTime});
+    };
 
-    function resetTimer() {
+    const resetTimer = () => {
         clearInterval(timerInterval);
         isTimerRunning = false;
         pausedTime = 0;
-        hourDisplay.innerText = '00';
-        minuteDisplay.innerText = '00';
-        secondDisplay.innerText = '00';
+        [hourDisplay, minuteDisplay, secondDisplay].forEach(display => display.innerText = '00');
         chrome.storage.local.remove(['activeTask', 'startTime', 'pausedTime']);
-    }
+    };
 
-    chrome.storage.local.get(['activeTask', 'startTime', 'pausedTime'], function(result) {
-        const activeTask = result.activeTask;
-        startTime = result.startTime;
+    const handleStartButtonClick = () => {
+        const taskTitle = startButton.closest('.ce-tasks__task').querySelector('.ce-task__title').innerText;
+
+        if (isTimerRunning) {
+            pauseTimer();
+        } else {
+            startTime = Date.now() - pausedTime * 1000;
+            chrome.storage.local.set({'activeTask': taskTitle, 'startTime': startTime});
+
+            startTimer(taskTitle);
+            isTimerRunning = true;
+        }
+        updateButtonDisplay();
+    };
+
+    const handlePauseButtonClick = () => {
+        if (isTimerRunning) {
+            pauseTimer();
+            updateButtonDisplay();
+        }
+    };
+
+    const handleEndButtonClick = () => {
+        resetTimer();
+        updateButtonDisplay();
+    };
+
+    const updateButtonDisplay = () => {
+        startButton.style.display = isTimerRunning ? 'none' : 'inline-block';
+        pauseButton.style.display = isTimerRunning ? 'inline-block' : 'none';
+    };
+
+    const getUsername = () => localStorage.getItem('username') || 'Guest User';
+
+    const setInitialUsername = () => {
+        const username = getUsername();
+        const usernameElement = document.querySelector('.ce-timetracking__username');
+        if (usernameElement) {
+            usernameElement.textContent = username;
+        }
+    };
+
+    startButton.addEventListener('click', handleStartButtonClick);
+    pauseButton.addEventListener('click', handlePauseButtonClick);
+    endButton.addEventListener('click', handleEndButtonClick);
+
+    setInitialUsername();
+
+    chrome.storage.local.get(['activeTask', 'startTime', 'pausedTime'], ({
+                                                                             activeTask,
+                                                                             startTime: storedStartTime,
+                                                                             pausedTime: storedPausedTime
+                                                                         }) => {
+        startTime = storedStartTime || Date.now();
+        pausedTime = storedPausedTime || 0;
 
         if (activeTask && startTime) {
             const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
-            pausedTime = result.pausedTime || 0;
 
             if (isTimerRunning || (elapsedTime - pausedTime > 0 && pausedTime !== 0)) {
                 startTimer(activeTask);
                 isTimerRunning = true;
-                startButton.style.display = 'none';
-                pauseButton.style.display = 'inline-block';
             } else if (pausedTime !== 0) {
-                hourDisplay.innerText = String(Math.floor(pausedTime / 3600)).padStart(2, '0');
-                minuteDisplay.innerText = String(Math.floor((pausedTime % 3600) / 60)).padStart(2, '0');
-                secondDisplay.innerText = String(pausedTime % 60).padStart(2, '0');
+                updateTimerDisplay();
             }
         }
+        updateButtonDisplay();
     });
-
-    startButton.addEventListener('click', function() {
-        const taskTitle = this.closest('.ce-tasks__task').querySelector('.ce-task__title').innerText;
-
-        if (isTimerRunning) {
-            pauseTimer();
-            startButton.style.display = 'inline-block';
-            pauseButton.style.display = 'none';
-        } else {
-            startTime = Date.now() - pausedTime * 1000; // Adjust startTime based on pausedTime
-            chrome.storage.local.set({ 'activeTask': taskTitle, 'startTime': startTime });
-
-            startTimer(taskTitle);
-            isTimerRunning = true;
-            startButton.style.display = 'none';
-            pauseButton.style.display = 'inline-block';
-        }
-    });
-
-    pauseButton.addEventListener('click', function() {
-        if (isTimerRunning) {
-            pauseTimer();
-            startButton.style.display = 'inline-block';
-            pauseButton.style.display = 'none';
-        }
-    });
-
-    endButton.addEventListener('click', function() {
-        resetTimer();
-        startButton.style.display = 'inline-block';
-        pauseButton.style.display = 'none';
-    });
-
-    let username = localStorage.getItem('username');
-
-    if (!username) {
-        username = 'Guest User';
-    }
-
-    const usernameElement = document.querySelector('.ce-timetracking__username');
-
-    if (usernameElement) {
-        usernameElement.textContent = username;
-    }
 });
