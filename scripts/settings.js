bcbc71053eee708c86edd7e930210dc277a8bee6
Mohamed Abdelkader <madsaaeq@gmail.com>
2024-01-01 10:50:24 +0200

    Refactor & add functionality to settings

---
+++ scripts/settings.js
@@ -0,0 +1,36 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const logoutButton = document.getElementById('logout');
+
+    logoutButton.addEventListener('click', async () => {
+        try {
+            const token = localStorage.getItem('accessToken');
+
+            const response = await fetch('https://dev.contenterp.com/api/v2/auth/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({ token })
+            });
+
+            if (response.ok) {
+                const responseData = await response.json();
+
+                if (responseData.code === 200) {
+                    // Logout successful
+                    localStorage.removeItem('accessToken');
+                    window.location.href = 'index.html';
+                } else {
+                    console.error('Logout failed:', responseData.message); // Handle unsuccessful logout
+                }
+            } else {
+                console.error('Error:', response.status); // Handle other HTTP errors
+            }
+        } catch (error) {
+            console.error('Error:', error); // Handle fetch or other errors
+        }
+    });
+});
